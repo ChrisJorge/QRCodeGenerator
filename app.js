d6b6c99f59092a0b13f96cffd1ffe5c33c5e0253
Chris <Christopherjorge47@icloud.com>
2024-05-03 18:17:56 -0400

    Completed functionality for download option

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 import { input, checkbox } from '@inquirer/prompts';
 import qrcode from 'qrcode';
 
+const option = (num,answer) => {
+    switch(num)
+    {
+        case 1:
+            qrcode.toString(answer,{type: 'terminal'}, (err,url) => {
+                console.log(url);
+            });
+            break;
+        case 2:
+            qrcode.toFile('./QRCODE.png', answer);
+            console.log('QRCode successfully created!');
+            break;
+    }
 
+}
 let answer =  await input({message: 'Enter the URL for the website'});
 let choice = await checkbox({message: 'Select How To View',
 choices: [
     {name: 'Terminal', value: 1},
     {name: "Download", value: 2},
-    {name: 'Terminal and Download', value: 3}
 ],
 });
 
-qrcode.toString(answer,{type: 'terminal'}, (err,url) => {
-    console.log(url)
-})
+choice.map((x) => {
+    option(x, answer);
+});

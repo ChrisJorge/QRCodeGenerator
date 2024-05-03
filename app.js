d6fc27c605b75493105ba4922b8af1305fd6ef20
Chris <Christopherjorge47@icloud.com>
2024-05-03 17:59:29 -0400

    added checkbox prompt

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // import inquirer from "inquirer";
-import { input } from '@inquirer/prompts';
+import { input, checkbox } from '@inquirer/prompts';
 import qrcode from 'qrcode';
 
 
 let answer =  await input({message: 'Enter the URL for the website'});
+let choice = await checkbox({message: 'Select How To View',
+choices: [
+    {name: 'Terminal', value: 1},
+    {name: "Download", value: 2},
+    {name: 'Terminal and Download', value: 3}
+],
+});
 
 qrcode.toString(answer,{type: 'terminal'}, (err,url) => {
     console.log(url)
